a49009f2c05d561906be33a6752468d305e9427e
SBDev1234 <sbdev1234@gmail.com>
2024-01-29 13:37:24 +0000

    update installer

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
     echo >&2 "$BASH_SOURCE error - sourced not invoked"
     return 1
 }
+
+
 cd "$(dirname $(realpath ${BASH_SOURCE}))/src" || exit
 
 find . -type f -exec cp -ruv --backup=t --parents -t $HOME {} \;
